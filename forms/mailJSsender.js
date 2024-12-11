5c10c2a75cda84cfdc9cf80f4256f08131d1f024
Saurabh Raj <66667012+Saurabhrajsaran1@users.noreply.github.com>
2024-12-11 18:40:55 +0530

    Add files via upload

---
+++ forms/mailJSsender.js
@@ -0,0 +1,9 @@
+async function sendMail() {
+    let params ={
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        message: document.getElementById('message').value,
+        subject: document.getElementById('subject').value
+    }
+    await emailjs.send('service_ygwl1pl','template_nftjmfb',params).then(alert("message sent"));
+}
